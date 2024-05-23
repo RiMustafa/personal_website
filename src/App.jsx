a755228dc5e37b3988297e2df3901c9e11535758
Riyan Mustafa <riyanmustafa@Riyans-Laptop.local>
2024-05-23 14:41:55 -0400

    Feat: update

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import './App.css';
 function App() {
   return (
     <>
+    <header className='header'>
+    Riyans Website
+    </header>
       <div className="section section-light-grey">
-        This is me testing
+        Hello I am Riyan! I am an aspiring software engineer, here is some of my work!
+      </div>
+      <div className="Github">
+        <a href="https://github.com/your-github-username" target="_blank" rel="noopener noreferrer">
+          <button className="github-button">Visit My GitHub</button>
+        </a>
       </div>
       <div className="section section-medium-grey">
       .
@@ -12,8 +20,8 @@ function App() {
       <div className="section section-dark-grey">
       .
       </div>
-    </>
-  );
+    </>  
+    );
 }
 
 export default App;
