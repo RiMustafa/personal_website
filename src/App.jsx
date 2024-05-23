070639acf6e308dfe32a31fd1ed14816242ce9d3
Riyan Mustafa <riyanmustafa@Riyans-Laptop.local>
2024-05-23 14:26:53 -0400

    feat: color

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
-<div>
-  hello
-</div>
+      <div className="section section-light-grey">
+        Section 1
+      </div>
+      <div className="section section-medium-grey">
+        Section 2
+      </div>
+      <div className="section section-dark-grey">
+        Section 3
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
