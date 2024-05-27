44b155776e67e0bffa0bae9545f6d3223f29e79b
Riyan Mustafa <riyanmustafa@Riyans-Laptop.local>
2024-05-26 20:24:42 -0400

    feat: update

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <header>
-    <nav>
+      <header id="head">
+    <nav id="menu">
       <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
@@ -19,12 +19,9 @@ const App = () => {
   <section id="home" className="hero">
         <h1>Riyan Mustafa</h1>
         <p>Aspiring Software Engineer</p>
-      </section>
-
-      <section>       
          <div className="social-buttons">
-          <a href="https://github.com/RiMustafa" target="_blank" rel="noopener noreferrer" className="social-button github">GitHub</a>
-          <a href="https://www.linkedin.com/in/riyan-mustafa-ab1207273/" target="_blank" rel="noopener noreferrer" className="social-button linkedin">LinkedIn</a>
+          <a href="https://github.com/RiMustafa" target="_blank" rel="noopener noreferrer" className="social-button github"><i class="fa-brands fa-github"></i></a>
+          <a href="https://www.linkedin.com/in/riyan-mustafa-ab1207273/" target="_blank" rel="noopener noreferrer" className="social-button linkedin"><i class="fa-brands fa-linkedin"></i></a>
         </div>
       </section>
 
@@ -54,19 +51,19 @@ const App = () => {
       </section>
 
       <section id="contact">
-        <h2>Contact</h2>
-        <form>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" required />
-          
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-          
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" required></textarea>
-          
-          <button type="submit">Submit</button>
-        </form>
+     <h2>Contact</h2>
+     <form>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required />
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required />
+
+    <label for="message">Message:</label>
+    <textarea id="message" name="message" required></textarea>
+    
+    <button type="submit">Submit</button>
+     </form>
       </section>
 
       <footer>
